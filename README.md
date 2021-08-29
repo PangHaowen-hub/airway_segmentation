765b8bf4661eca48fa60bfbfbf7ef5b2bb77f624
PangHaowen-hub <59225430+PangHaowen-hub@users.noreply.github.com>
2021-08-29 21:36:19 +0800

    8-29

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # airway_segmentation
-第一步：使用mmimics提取气道树，并导出为dicom
+第一步：使用mimics提取气道树，并导出为dicom
 
 第二步：运行dcm2nii.py将dicom转为nii
 
@@ -8,4 +8,6 @@
 第四步：运行nii_resample.py对图像进行重采样
 
 第五步：运行nii2npz.py将nii转为npz
+
+第六步：使用read_npz.py将stage34中segments.npz转为nii
 
--- read_npz.py
+++ read_npz.py
@@ -2,10 +2,10 @@ import numpy as np
 import SimpleITK as sitk
 
 
-data = np.load(r'D:\github_code\Airway-master\example_data\model2.npz')
-x = data['arr_0'].astype(int)
+data = np.load(r'lobes.npz')
+x = data['color_mask'].astype(int)
 print(x.shape)
 
 new_mask_img = sitk.GetImageFromArray(x)
 new_mask_img.SetSpacing((0.8, 0.8, 0.8))
-sitk.WriteImage(new_mask_img, r'D:\github_code\Airway-master\example_data\temp2.nii.gz')
+sitk.WriteImage(new_mask_img, r'color_mask.nii.gz')
